9e53a3863c60f3b5b629d27eb2ca1c569dab18ef
Gustavo Kuklinski <gustavo.kuklinski@gmail.com>
2014-01-22 23:51:35 -0500

    Test

--- travis.rb
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env ruby
-result = 'sass scss/scrath-bundle.scss css/scrath-bundle.css'
-raise result unless $?.to_i == 0
-raise "When compiled the module should output some CSS" unless File.exists?('css/scrath-bundle.css')
-puts "Regular compile worked successfully"
